45f7ac2b0fb6c96b5a1c9c58d73fc9b3ff941b9b
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-06-15 19:08:48 +0100

    html-loader, file-loader and clean-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -13,6 +13,22 @@ module.exports = {
           "sass-loader", // 1. Turns sass into css
         ],
       },
+      {
+        test: /\.html$/,
+        use: [
+          "html-loader", // Emits require("imagename.svg") from img tag in html
+        ],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader", // Loads the file require(imagename.svg)
+          options: {
+            name: "[name].[hash].[ext]", // ouput filename is webpack.1g2gg2.svg
+            outputPath: "imgs", // imgs dir created under target folder
+          },
+        },
+      },
     ],
   },
   plugins: [
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -8,4 +9,5 @@ module.exports = merge(common, {
     filename: "bundle.[contentHash].js",
     path: path.resolve(__dirname, "target"),
   },
+  plugins: [new CleanWebpackPlugin()],
 });
